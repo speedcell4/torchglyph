3cfedc1c06cd38b0bc4ca72f97e9f86c544b1194
speedcell4 <speedcell4@gmail.com>
2020-03-04 16:48:39 +0900

    Refactor: Update vocabulary updating

--- torchglyph/vocab.py
+++ torchglyph/vocab.py
@@ -1,16 +1,17 @@
 from collections import Counter, defaultdict
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 
 
 class Vocab(object):
     def __init__(self, counter: Counter,
-                 unk_token: Optional[str] = '<unk>',
+                 unk_token: Optional[str],
                  special_tokens: Tuple[Optional[str], ...] = (),
                  max_size: Optional[int] = None, min_freq: int = 1) -> None:
         super(Vocab, self).__init__()
 
         self.counter = counter
         self.unk_token = unk_token
+        self.special_tokens = special_tokens
         self.unk_idx = 0
         self.max_size = max_size
         self.min_freq = min_freq
@@ -26,7 +27,7 @@ class Vocab(object):
             if token is not None:
                 self.add_token_(token)
 
-        for token, freq in counter.most_common(n=max_size - len(self.stoi)):
+        for token, freq in counter.most_common(n=max_size):
             if freq < min_freq:
                 break
             self.add_token_(token)
@@ -37,57 +38,59 @@ class Vocab(object):
     def add_token_(self, token) -> int:
         assert token is not None
 
-        idx = len(self.itos)
-        self.stoi[token] = idx
-        self.itos.append(token)
-        return idx
+        if token not in self.stoi:
+            self.stoi[token] = len(self.itos)
+            self.itos.append(token)
+
+        return self.stoi[token]
 
     def __repr__(self) -> str:
         args = ', '.join([
             f'size={self.__len__()}',
             f'unk_token={self.unk_token}',
-            f'max_size={self.max_size}',
-            f'min_freq={self.min_freq}',
         ])
         return f'{self.__class__.__name__}({args})'
 
     def __len__(self) -> int:
         return len(self.stoi)
 
-    def __and__(self, other: 'Vocab') -> 'Vocab':
+    def __and__(self, rhs: Union['Counter', 'Vocab']) -> 'Vocab':
+        if isinstance(rhs, Vocab):
+            rhs = rhs.counter
         counter = Counter({
             token: freq
             for token, freq in self.counter.items()
-            if token in other.counter
+            if token in rhs
         })
         return Vocab(
             counter=counter,
-            unk_token=self.unk_token,
-            max_size=self.max_size,
-            min_freq=self.min_freq,
+            unk_token=self.unk_token, special_tokens=self.special_tokens,
+            max_size=self.max_size, min_freq=self.min_freq,
         )
 
-    def __add__(self, other: 'Vocab') -> 'Vocab':
+    def __add__(self, rhs: Union['Counter', 'Vocab']) -> 'Vocab':
+        if isinstance(rhs, Vocab):
+            rhs = rhs.counter
         counter = Counter({
-            token: self.counter[token] + other.counter[token]
-            for token in {*self.counter.keys(), *other.counter.keys()}
+            token: self.counter[token] + rhs[token]
+            for token in {*self.counter.keys(), *rhs.keys()}
         })
         return Vocab(
             counter=counter,
-            unk_token=self.unk_token,
-            max_size=self.max_size,
-            min_freq=self.min_freq,
+            unk_token=self.unk_token, special_tokens=self.special_tokens,
+            max_size=self.max_size, min_freq=self.min_freq,
         )
 
-    def __sub__(self, other: 'Vocab') -> 'Vocab':
+    def __sub__(self, rhs: Union['Counter', 'Vocab']) -> 'Vocab':
+        if isinstance(rhs, Vocab):
+            rhs = rhs.counter
         counter = Counter({
             token: freq
             for token, freq in self.counter.items()
-            if token not in other.counter
+            if token not in rhs
         })
         return Vocab(
             counter=counter,
-            unk_token=self.unk_token,
-            max_size=self.max_size,
-            min_freq=self.min_freq,
+            unk_token=self.unk_token, special_tokens=self.special_tokens,
+            max_size=self.max_size, min_freq=self.min_freq,
         )
