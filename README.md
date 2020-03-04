22070a556244d62d5d598c52862a60eb6c1ed2cc
speedcell4 <speedcell4@gmail.com>
2020-03-04 15:37:42 +0900

    Feat: Add basic files

---
+++ README.md
@@ -0,0 +1,7 @@
+# TorchGlyph
+
+[![Actions Status](https://github.com/speedcell4/torchglyph/workflows/unit-tests/badge.svg)](https://github.com/speedcell4/torchglyph/actions)
+
+## requirements
+
+- Python3.7 or higher
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fp:
+    long_description = fp.read()
+
+setup(
+    name='torchglyph',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/speedcell4/torchglyph',
+    license='MIT',
+    author='speedcell4',
+    author_email='speedcell4@gmail.com',
+    description='Data processing utilities for natural language processing',
+    long_description=long_description,
+)
