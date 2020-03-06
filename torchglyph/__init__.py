1c17fdfeae3d621d695b9c36c06d26ef7ec8438a
speedcell4 <speedcell4@gmail.com>
2020-03-06 20:38:01 +0900

    Feat: import things from root

--- torchglyph/__init__.py
+++ torchglyph/__init__.py
@@ -3,3 +3,8 @@ from pathlib import Path
 data_path = Path.home() / '.torchglyph'
 if not data_path.exists():
     data_path.mkdir(parents=True, exist_ok=True)
+
+from torchglyph.vocab import Vocab, Vectors, Glove
+from torchglyph.dataset import Dataset, DataLoader
+from torchglyph.pipe import *
+from torchglyph.proc import *
