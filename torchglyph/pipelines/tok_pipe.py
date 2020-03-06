09ab479d241672e6e597a9d14488a34c03c5d510
speedcell4 <speedcell4@gmail.com>
2020-03-06 14:37:58 +0900

    Feat: Add PaddedTokPipe

--- torchglyph/pipelines/tok_pipe.py
+++ torchglyph/pipelines/tok_pipe.py
@@ -1,5 +1,15 @@
 from torchglyph.dataset import Pipeline
-from torchglyph.proc import ToLength, ToTensor
+from torchglyph.proc import ToLength, ToTensor, AddToCounter, BuildVocab, Numbering
+
+
+class PaddedTokPipe(Pipeline):
+    def __init__(self) -> None:
+        super(PaddedTokPipe, self).__init__(
+            pre_procs=AddToCounter(),
+            vocab_procs=BuildVocab(pad_token=None),
+            post_procs=Numbering(),
+            batch_procs=ToTensor(),
+        )
 
 
 class SeqLengthPipe(Pipeline):
