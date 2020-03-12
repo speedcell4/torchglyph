0bb5b1118d9882d699db5faacd3b5a8dbf0409e7
speedcell4 <speedcell4@gmail.com>
2020-03-12 22:45:13 +0900

    Feat: Add SupportPack

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -3,7 +3,7 @@ from hypothesis import given, strategies as st
 from torch import nn
 from torch.nn.utils.rnn import pack_sequence
 
-from torchglyph.functional import packing
+from torchglyph.functional import support_pack
 
 
 @given(
@@ -12,7 +12,7 @@ from torchglyph.functional import packing
     output_dim=st.integers(1, 20),
 )
 def test_packing(batch_sizes, input_dim, output_dim):
-    layer = packing(nn.Linear(input_dim, output_dim))
+    layer = support_pack(nn.Linear(input_dim, output_dim))
 
     x1 = torch.rand(*batch_sizes, input_dim)
     y1 = layer(x1)
--- torchglyph/functional.py
+++ torchglyph/functional.py
@@ -1,11 +1,13 @@
-from typing import Union
+import functools
+from typing import Union, Tuple, Dict, Any
 
 import torch
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
 
 
-def packing(fn):
+def support_pack(fn):
+    @functools.wraps(fn)
     def wrap(x: Union[Tensor, PackedSequence], *args, **kwargs) -> Union[Tensor, PackedSequence]:
         if torch.is_tensor(x):
             return fn(x, *args, **kwargs)
@@ -13,3 +15,17 @@ def packing(fn):
             return x._replace(data=fn(x.data, *args, **kwargs))
 
     return wrap
+
+
+class SupportPack(type):
+    def __new__(cls, name: str, bases: Tuple[type, ...], attrs: Dict[str, Any]):
+        forward_fn = bases[0].forward
+
+        @functools.wraps(forward_fn)
+        def wrap(self, x: Union[Tensor, PackedSequence], *args, **kwargs) -> Union[Tensor, PackedSequence]:
+            if torch.is_tensor(x):
+                return forward_fn(self, x, *args, **kwargs)
+            else:
+                return x._replace(data=forward_fn(self, x.data, *args, **kwargs))
+
+        return type(name, bases, {**attrs, 'forward': wrap})
