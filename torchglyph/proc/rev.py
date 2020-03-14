9979f367f79203da5f618a93d69e60e80c73e913
speedcell4 <speedcell4@gmail.com>
2020-03-14 19:19:23 +0900

    Refactor: Remove RevVocab

--- torchglyph/proc/rev.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-from torchglyph.proc.abc import Proc
-from torchglyph.vocab import Vocab
-
-
-class RevVocab(Proc):
-    def __call__(self, data: List[int], vocab: Vocab, *args, **kwargs) -> List[str]:
-        return [vocab.itos[datum] for datum in data]
