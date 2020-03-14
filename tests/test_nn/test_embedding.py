587e5c208a919972896edb92795825bb24297e2e
speedcell4 <speedcell4@gmail.com>
2020-03-14 18:15:58 +0900

    Test: Add unit test for TokEmbedding

--- tests/test_nn/test_embedding.py
+++ tests/test_nn/test_embedding.py
@@ -2,8 +2,35 @@ import torch
 from hypothesis import given, strategies as st
 from torch.nn.utils.rnn import pack_padded_sequence
 
-from tests.test_nn.corpora import SubCorpus, SENTENCES
-from torchglyph.nn import SubLstmEmbedding
+from tests.test_nn.corpora import SeqCorpus, SubCorpus, SENTENCES
+from torchglyph.nn import TokEmbedding, SubLstmEmbedding
+
+
+@given(
+    batch_first=st.booleans(),
+    batch_size=st.integers(1, 12),
+    char_dim=st.integers(1, 12),
+    sentences=SENTENCES,
+)
+def test_tok_embedding(batch_first, batch_size, char_dim, sentences):
+    loader, = SeqCorpus.new(sentences=sentences, batch_size=batch_size, batch_first=batch_first)
+    layer = TokEmbedding(
+        num_embeddings=len(loader.vocabs.pad), embedding_dim=char_dim,
+    )
+
+    for batch in loader:
+        pad_encoding = layer(batch.pad)
+        pad_encoding = pack_padded_sequence(
+            pad_encoding, batch.seq_length,
+            batch_first=batch_first, enforce_sorted=False,
+        )
+
+        pack_encoding = layer(batch.pack)
+
+        assert torch.allclose(pad_encoding.data, pack_encoding.data, atol=1e-5)
+        assert torch.allclose(pad_encoding.batch_sizes, pack_encoding.batch_sizes)
+        assert torch.allclose(pad_encoding.sorted_indices, pack_encoding.sorted_indices)
+        assert torch.allclose(pad_encoding.unsorted_indices, pack_encoding.unsorted_indices)
 
 
 @given(
