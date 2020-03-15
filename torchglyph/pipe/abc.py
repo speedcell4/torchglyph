d31664ee2dbb67e19de7be916e11affab1f58f2b
speedcell4 <speedcell4@gmail.com>
2020-03-16 02:51:14 +0900

    Feat: Add __call__ for Pipe

--- torchglyph/pipe/abc.py
+++ torchglyph/pipe/abc.py
@@ -81,6 +81,17 @@ class Pipe(object, metaclass=ABCMeta):
                              f"instead of '{Vocab.__name__}'")
         return self
 
+    def __call__(self, data: List[Any]) -> Tuple[Any, Vocab]:
+        counter = Counter()
+        data = [self._pre_proc(datum, counter=counter) for datum in data]
+        vocab = self._vocab_proc(
+            counter, name='unnamed',
+            special_tokens=(),
+            max_size=None, min_freq=1,
+        )
+        data = [self._post_proc(datum, vocab=vocab) for datum in data]
+        return self._batch_proc(data, vocab=vocab), vocab
+
     def collate_fn(self, collected_ins: List[Any]) -> Any:
         return self._batch_proc(collected_ins, vocab=self.vocab)
 
