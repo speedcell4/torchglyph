f1f7d8b4e7ab1bafecbfcccac1a298e5d50563c6
speedcell4 <speedcell4@gmail.com>
2020-03-15 14:10:45 +0900

    Test: Hypothesis-rize max_sentence_length

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -8,10 +8,11 @@ from torchglyph.functional import support_pack
 
 @given(
     batch_sizes=st.lists(st.integers(1, 10), min_size=1, max_size=4),
+    max_sentence_length=st.integers(2, 20),
     input_dim=st.integers(1, 20),
     output_dim=st.integers(1, 20),
 )
-def test_packing(batch_sizes, input_dim, output_dim):
+def test_packing(batch_sizes, max_sentence_length, input_dim, output_dim):
     layer = support_pack(nn.Linear(input_dim, output_dim))
 
     x1 = torch.rand(*batch_sizes, input_dim)
@@ -20,7 +21,7 @@ def test_packing(batch_sizes, input_dim, output_dim):
 
     batch_size = sum(batch_sizes)
     x2 = pack_sequence([
-        torch.randn((torch.randint(1, 12, ()).item(), input_dim))
+        torch.randn((torch.randint(1, max_sentence_length, ()).item(), input_dim))
         for _ in range(batch_size)
     ], enforce_sorted=False)
     y2 = layer(x2)
