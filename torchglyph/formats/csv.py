eb33b0e1f5be304995874e5e8a720aa56cc996fb
speedcell4 <speedcell4@gmail.com>
2020-03-17 16:26:28 +0900

    Style: Add type annotation for csv

--- torchglyph/formats/csv.py
+++ torchglyph/formats/csv.py
@@ -8,18 +8,20 @@ from torchglyph.io import open_io
 Sentence = RawSentence = Tuple[Union[str, int]]
 
 
-def load(f: IO, *, sep=',', quotechar='"', mode: str = 'r', encoding: str = 'utf-8') -> Iterable[RawSentence]:
+def load(f: IO, *, sep: str = ',', quotechar: str = '"',
+         mode: str = 'r', encoding: str = 'utf-8') -> Iterable[RawSentence]:
     with open_io(f, mode=mode, encoding=encoding) as fp:
         yield from csv.reader(fp, delimiter=sep, quotechar=quotechar)
 
 
-def dumps(sentence: Sentence, *, sep=',', quotechar='"') -> str:
+def dumps(sentence: Sentence, *, sep: str = ',', quotechar: str = '"') -> str:
     with StringIO(initial_value='') as s:
         fp = csv.writer(s, delimiter=sep, quotechar=quotechar)
         fp.writerow(sentence)
         return s.getvalue()
 
 
-def dump(sentence: Sentence, f: IO, *, sep=',', quotechar='"', mode: str = 'w', encoding: str = 'utf-8') -> None:
+def dump(sentence: Sentence, f: IO, *, sep: str = ',', quotechar: str = '"',
+         mode: str = 'w', encoding: str = 'utf-8') -> None:
     with open_io(f, mode=mode, encoding=encoding) as fp:
         print(dumps(sentence, sep=sep, quotechar=quotechar), file=fp)
