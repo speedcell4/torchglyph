98b71344ba1b68276e81ab268f5d9b6cf7b9f233
speedcell4 <speedcell4@gmail.com>
2020-05-03 18:25:22 +0900

    Fix: Resolve toggle_loggers import path

--- torchglyph/proc/tokenizer.py
+++ torchglyph/proc/tokenizer.py
@@ -5,14 +5,15 @@ from allennlp.data import Token as AllenToken, Instance as AllenInstance
 from allennlp.data.fields import TextField as AllenTextField
 from allennlp.data.token_indexers import ELMoTokenCharactersIndexer
 
-from torchglyph.proc import Proc, toggle_loggers
+from torchglyph.io import toggle_loggers
+from torchglyph.proc import Proc
 
 toggle_loggers('allennlp', False)
 toggle_loggers('transformers', False)
 
 
 class ELMoTokenizer(Proc):
-    def __init__(self):
+    def __init__(self) -> None:
         super(ELMoTokenizer, self).__init__()
         self.tokenizer = ELMoTokenCharactersIndexer()
 
@@ -36,66 +37,66 @@ class TransformerTokenizerProc(Proc):
 
 
 class BertTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'bert-base-uncased'):
+    def __init__(self, weight: str = 'bert-base-uncased') -> None:
         super(BertTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.BertTokenizer.from_pretrained(weight)
 
 
 class OpenAIGPTTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'openai-gpt'):
+    def __init__(self, weight: str = 'openai-gpt') -> None:
         super(OpenAIGPTTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.OpenAIGPTTokenizer.from_pretrained(weight)
 
 
 class GPT2Tokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'gpt2'):
+    def __init__(self, weight: str = 'gpt2') -> None:
         super(GPT2Tokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.GPT2Tokenizer.from_pretrained(weight)
 
 
 class CTRLTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'ctrl'):
+    def __init__(self, weight: str = 'ctrl') -> None:
         super(CTRLTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.CTRLTokenizer.from_pretrained(weight)
 
 
 class TransfoXLTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'transfo-xl-wt103'):
+    def __init__(self, weight: str = 'transfo-xl-wt103') -> None:
         super(TransfoXLTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.TransfoXLTokenizer.from_pretrained(weight)
 
 
 class XLNetTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'xlnet-base-cased'):
+    def __init__(self, weight: str = 'xlnet-base-cased') -> None:
         super(XLNetTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.XLNetTokenizer.from_pretrained(weight)
 
 
 class XLMTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'xlm-mlm-enfr-1024'):
+    def __init__(self, weight: str = 'xlm-mlm-enfr-1024') -> None:
         super(XLMTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.XLMTokenizer.from_pretrained(weight)
 
 
 class DistilBertTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'distilbert-base-cased'):
+    def __init__(self, weight: str = 'distilbert-base-cased') -> None:
         super(DistilBertTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.DistilBertTokenizer.from_pretrained(weight)
 
 
 class RobertaTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'roberta-base'):
+    def __init__(self, weight: str = 'roberta-base') -> None:
         super(RobertaTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.RobertaTokenizer.from_pretrained(weight)
 
 
 class XLMRobertaTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'xlm-roberta-base'):
+    def __init__(self, weight: str = 'xlm-roberta-base') -> None:
         super(XLMRobertaTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.XLMRobertaTokenizer.from_pretrained(weight)
 
 
 class BartTokenizer(TransformerTokenizerProc):
-    def __init__(self, weight: str = 'bart-large'):
+    def __init__(self, weight: str = 'bart-large') -> None:
         super(BartTokenizer, self).__init__(weight=weight)
         self.tokenizer = transformers.BartTokenizer.from_pretrained(weight)
