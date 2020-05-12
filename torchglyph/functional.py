11a347d0ff9de4a455e40d63810042c40007cff7
speedcell4 <speedcell4@gmail.com>
2020-05-12 15:50:30 +0900

    Feat: Add head_pack and prepend_pack

--- torchglyph/functional.py
+++ torchglyph/functional.py
@@ -1,5 +1,6 @@
 import functools
-from typing import Any, Union, Tuple, Dict
+from typing import Any
+from typing import Union, Tuple, Dict
 
 import torch
 from torch import Tensor
@@ -29,3 +30,16 @@ class SupportPack(type):
                 return x._replace(data=forward_fn(self, x.data, *args, **kwargs))
 
         return type(name, bases, {**attrs, 'forward': forward})
+
+
+def head_pack(pack: PackedSequence) -> Tensor:
+    return pack.data[:pack.batch_sizes[0].item()]
+
+
+def prepend_pack(pack: PackedSequence, value: Union[int, bool, float, Tensor]) -> PackedSequence:
+    if not torch.is_tensor(value):
+        value = torch.full_like(head_pack(pack), fill_value=value)
+    return pack._replace(
+        data=torch.cat([value, pack.data], dim=0),
+        batch_sizes=torch.cat([pack.batch_sizes[:1], pack.batch_sizes], dim=0),
+    )
