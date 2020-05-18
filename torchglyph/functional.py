90a3b142864b706ed527615f770fcb1911c87a04
speedcell4 <speedcell4@gmail.com>
2020-05-18 12:57:03 +0900

    Feat: Add tail_pack and append_pack

--- torchglyph/functional.py
+++ torchglyph/functional.py
@@ -5,7 +5,8 @@ from typing import Union, Tuple, Dict
 import torch
 from torch import Tensor
 from torch import nn
-from torch.nn.utils.rnn import PackedSequence
+from torch.nn.utils.rnn import PackedSequence, pad_packed_sequence
+from torch.nn.utils.rnn import pack_padded_sequence
 
 
 def support_pack(fn):
@@ -55,4 +56,21 @@ def prepend_pack(pack: PackedSequence, value: Union[int, bool, float, Tensor]) -
     return pack._replace(
         data=torch.cat([value, pack.data], dim=0),
         batch_sizes=torch.cat([pack.batch_sizes[:1], pack.batch_sizes], dim=0),
+    )
+
+
+def tail_pack(pack: PackedSequence) -> Tensor:
+    data, lengths = pad_packed_sequence(pack, batch_first=True)  # type: (Tensor, Tensor)
+    indices = torch.arange(lengths.size(0), dtype=torch.long, device=data.device)
+    return data[indices, lengths - 1]
+
+
+def append_pack(pack: PackedSequence, value: Union[int, bool, float, Tensor]) -> PackedSequence:
+    if not torch.is_tensor(value):
+        value = torch.full_like(head_pack(pack), fill_value=value)
+    data, lengths = pad_packed_sequence(pack, batch_first=True)  # type: (Tensor, Tensor)
+    indices = torch.arange(lengths.size(0), dtype=torch.long, device=data.device)
+    return pack_padded_sequence(
+        torch.cat([data, value[:, None]], dim=1).index_put((indices, lengths), value),
+        lengths + 1, batch_first=True, enforce_sorted=False,
     )
