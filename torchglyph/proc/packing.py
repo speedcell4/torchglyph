d32328edd75e31fc3d9cd43187d85a525b49e254
speedcell4 <speedcell4@gmail.com>
2021-06-26 03:34:03 +0900

    Feat: Add PackListList

--- torchglyph/proc/packing.py
+++ torchglyph/proc/packing.py
@@ -1,15 +1,37 @@
-from typing import List
+from typing import List, Optional, Tuple
 
+import torch
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
-from torchrua import pack_sequence
+from torchrua import pack_sequence, reduce_catted_sequences
 
 from torchglyph.proc import Proc
 
+__all__ = [
+    'PackList',
+    'PackListList',
+]
+
 
 class PackList(Proc):
-    def __init__(self) -> None:
+    def __init__(self, device: Optional[torch.device] = None) -> None:
         super(PackList, self).__init__()
+        self.device = device
+
+    def extra_repr(self) -> str:
+        return f'device={self.device}'
 
     def __call__(self, sequences: List[Tensor], **kwargs) -> PackedSequence:
-        return pack_sequence(sequences)
+        return pack_sequence(sequences, device=self.device)
+
+
+class PackListList(Proc):
+    def __init__(self, device: Optional[torch.device] = None) -> None:
+        super(PackListList, self).__init__()
+        self.device = device
+
+    def extra_repr(self) -> str:
+        return f'device={self.device}'
+
+    def __call__(self, sequences: List[Tuple[Tensor, Tensor]], **kwargs) -> PackedSequence:
+        return reduce_catted_sequences(sequences, device=self.device)
