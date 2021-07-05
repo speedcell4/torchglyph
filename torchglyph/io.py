10f43cd16158780de5128404952411b8c9b373a9
speedcell4 <speedcell4@gmail.com>
2021-07-05 20:22:41 +0900

    Refactor: Remove old unit tests

--- torchglyph/io.py
+++ torchglyph/io.py
@@ -5,82 +5,45 @@ import re
 import shutil
 import tarfile
 import zipfile
-from contextlib import contextmanager
 from pathlib import Path
-from typing import Union, TextIO, Pattern
+from typing import Union, Pattern
 from urllib.request import urlretrieve
 
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
-IO = Union[str, Path, TextIO]
 
+def download_and_unzip(url: str, path: Path) -> Path:
+    if path.exists():
+        return path
 
-@contextmanager
-def open_io(f: IO, mode: str, encoding: str):
-    try:
-        if isinstance(f, (str, Path)):
-            with open(f, mode=mode, encoding=encoding) as fp:
-                yield fp
-        else:
-            yield f
-    finally:
-        pass
-
-
-# copied and modified from https://github.com/pytorch/text
-def reporthook(t):
-    """https://github.com/tqdm/tqdm"""
-    last_b = [0]
-
-    def inner(b=1, bsize=1, tsize=None) -> None:
-        """
-        b: int, optional
-        Number of blocks just transferred [default: 1].
-        bsize: int, optional
-        Size of each block (in tqdm units) [default: 1].
-        tsize: int, optional
-        Total size (in tqdm units). If [default: None] remains unchanged.
-        """
-        if tsize is not None:
-            t.total = tsize
-        t.update((b - last_b[0]) * bsize)
-        last_b[0] = b
-
-    return inner
-
-
-# copied and modified from https://github.com/pytorch/text
-def download_and_unzip(url: str, dest: Path) -> Path:
-    if dest.exists():
-        return dest
-
-    if not dest.parent.exists():
-        dest.parent.mkdir(parents=True, exist_ok=True)
-
-    with tqdm(unit='B', unit_scale=True, miniters=1, desc=f'downloading {dest}') as t:
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with tqdm(unit='B', unit_scale=True, miniters=1, desc=f'downloading {path}') as t:
         try:
-            urlretrieve(url, str(dest), reporthook=reporthook(t))
+            urlretrieve(url, str(path), reporthook=reporthook(t))
         except KeyboardInterrupt as err:  # remove the partial zip file
-            os.remove(str(dest))
+            os.remove(str(path))
             raise err
 
-    if dest.suffix == '.zip':
-        logger.info(f'extracting {dest}')
-        with zipfile.ZipFile(dest, "r") as fp:
-            fp.extractall(path=dest.parent)
-    elif dest.suffixes[-2:] == ['.tar', '.gz']:
-        logger.info(f'extracting {dest}')
-        with tarfile.open(dest, 'r:gz') as fp:
-            fp.extractall(path=dest.parent)
-    elif dest.suffix == '.gz':
-        logger.info(f'extracting {dest}')
-        with gzip.open(dest, mode='rb') as fs:
-            with dest.with_suffix('').open(mode='wb') as fd:
-                shutil.copyfileobj(fs, fd)
+    return extract_files(path)
 
-    return dest
+
+def extract_files(path: Path) -> Path:
+    if path.suffix == '.zip':
+        logger.info(f'extracting {path}')
+        with zipfile.ZipFile(path, "r") as fp:
+            fp.extractall(path=path.parent)
+    elif path.suffixes[-2:] == ['.tar', '.gz']:
+        logger.info(f'extracting {path}')
+        with tarfile.open(path, 'r:gz') as fp:
+            fp.extractall(path=path.parent)
+    elif path.suffix == '.gz':
+        logger.info(f'extracting {path}')
+        with gzip.open(path, mode='rb') as fs:
+            with path.with_suffix('').open(mode='wb') as fd:
+                shutil.copyfileobj(fs, fd)
+    return path
 
 
 def toggle_loggers(pattern: Union[str, Pattern], enable: bool) -> None:
