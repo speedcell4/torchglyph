6dc0d3644fd27b4c8df5a3b890f39528bdff6d32
speedcell4 <speedcell4@gmail.com>
2021-07-08 10:23:19 +0900

    Fix: CharLstmEmbedding pooling operation

--- torchglyph/nn/embedding.py
+++ torchglyph/nn/embedding.py
@@ -1,4 +1,4 @@
-from einops.layers.torch import Reduce
+from einops import rearrange
 from torch import nn, Tensor
 from torch.nn.utils.rnn import PackedSequence
 from torchrua import PackedMeta, PackedSequential
@@ -16,7 +16,7 @@ class TokenEmbedding(nn.Embedding, metaclass=PackedMeta):
 class CharLstmEmbedding(nn.Module):
     def __init__(self, hidden_dim: int = 50, num_layers: int = 1,
                  bias: bool = True, bidirectional: bool = True,
-                 dropout: float = 0.5, reduction: str = 'max', *,
+                 dropout: float = 0.5, *,
                  char_embedding: TokenEmbedding) -> None:
         super(CharLstmEmbedding, self).__init__()
 
@@ -34,9 +34,8 @@ class CharLstmEmbedding(nn.Module):
 
         self.num_directions = 2 if self.rnn.bidirectional else 1
         self.embedding_dim = self.rnn.hidden_size * self.num_directions
-        self.reduce = Reduce('(d l) b x -> b (d x)', reduction=reduction, d=self.num_directions)
 
     def forward(self, indices: PackedSequence) -> Tensor:
         embedding = self.embedding(indices)
         _, (encoding, _) = self.rnn(embedding)
-        return self.reduce(encoding)
+        return rearrange(encoding, '(l d) b x -> l b (d x)', d=self.num_directions)[-1]
