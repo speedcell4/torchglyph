7c56df696227d584cf54ba98d00b2129f502abff
speedcell4 <speedcell4@gmail.com>
2021-07-19 20:06:24 +0900

    Feat: Update interface of TokenEmbedding

--- torchglyph/nn/embedding.py
+++ torchglyph/nn/embedding.py
@@ -1,6 +1,7 @@
 from einops import rearrange
 from torch import nn, Tensor
 from torch.nn.utils.rnn import PackedSequence
+from torchglyph.vocab import Vocab
 from torchrua import PackedMeta, PackedSequential
 
 __all__ = [
@@ -10,13 +11,33 @@ __all__ = [
 
 
 class TokenEmbedding(nn.Embedding, metaclass=PackedMeta):
-    pass
+    def __init__(self, embedding_dim: int, freeze: bool = False, *, vocab: Vocab = None,
+                 num_embeddings: int = 0, padding_idx: int = None) -> None:
+        if vocab is not None:
+            super(TokenEmbedding, self).__init__(
+                embedding_dim=embedding_dim,
+                num_embeddings=len(vocab),
+                padding_idx=vocab.pad_idx,
+                _weight=vocab.vectors,
+            )
+        else:
+            super(TokenEmbedding, self).__init__(
+                embedding_dim=embedding_dim,
+                num_embeddings=num_embeddings,
+                padding_idx=padding_idx,
+                _weight=None,
+            )
+        self.weight.requires_grad = not freeze
+
+    def extra_repr(self) -> str:
+        if not self.weight.requires_grad:
+            return ', '.join([super(TokenEmbedding, self).extra_repr(), 'frozen'])
+        return super(TokenEmbedding, self).extra_repr()
 
 
 class CharLstmEmbedding(nn.Module):
     def __init__(self, hidden_dim: int = 50, num_layers: int = 1,
-                 bias: bool = True, bidirectional: bool = True,
-                 dropout: float = 0.5, *,
+                 bias: bool = True, bidirectional: bool = True, dropout: float = 0.5, *,
                  char_embedding: TokenEmbedding) -> None:
         super(CharLstmEmbedding, self).__init__()
 
