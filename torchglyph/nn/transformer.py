cb64d429b599e52b033474fd1db3687fc80789da
speedcell4 <speedcell4@gmail.com>
2021-09-03 01:58:22 +0900

    Feat: Add TransformerDecoderLayer

--- torchglyph/nn/transformer.py
+++ torchglyph/nn/transformer.py
@@ -7,6 +7,7 @@ from torchglyph.nn.attention import MultiHeadAttention
 __all__ = [
     'TransformerFfn',
     'TransformerEncoderLayer',
+    'TransformerDecoderLayer',
 ]
 
 
@@ -60,10 +61,51 @@ class TransformerEncoderLayer(nn.Module):
 
         self.norm1 = nn.LayerNorm(in_size)
         self.norm2 = nn.LayerNorm(in_size)
-        self.dropout1 = nn.Dropout(ffn_dropout)
-        self.dropout2 = nn.Dropout(ffn_dropout)
+        self.dropout = nn.Dropout(ffn_dropout)
 
     def forward(self, src: Tensor, mask: Optional[Tensor] = None) -> Tensor:
-        src = self.norm1(src + self.dropout1(self.att(q=src, k=src, v=src, mask=mask)))
-        src = self.norm2(src + self.dropout2(self.ffn(src)))
+        src = self.norm1(src + self.dropout(self.att(q=src, k=src, v=src, mask=mask)))
+        src = self.norm2(src + self.dropout(self.ffn(src)))
         return src
+
+
+class TransformerDecoderLayer(nn.Module):
+    def __init__(self, hidden_size: int = 2048, num_heads: int = 8,
+                 att_dropout: float = 0., ffn_dropout: float = 0., bias: bool = True, *,
+                 in_size: int) -> None:
+        super(TransformerDecoderLayer, self).__init__()
+
+        self.in_size = in_size
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.att_dropout = att_dropout
+        self.ffn_dropout = ffn_dropout
+        self.bias = bias
+
+        self.tgt = MultiHeadAttention(
+            num_heads=num_heads,
+            head_dim=in_size // num_heads,
+            dropout=att_dropout, bias=bias,
+            q_dim=in_size, k_dim=in_size, v_dim=in_size,
+        )
+        self.src = MultiHeadAttention(
+            num_heads=num_heads,
+            head_dim=in_size // num_heads,
+            dropout=att_dropout, bias=bias,
+            q_dim=in_size, k_dim=in_size, v_dim=in_size,
+        )
+        self.ffn = TransformerFfn(
+            dropout=ffn_dropout, bias=bias,
+            in_size=in_size, hidden_size=hidden_size,
+        )
+
+        self.norm1 = nn.LayerNorm(in_size)
+        self.norm2 = nn.LayerNorm(in_size)
+        self.norm3 = nn.LayerNorm(in_size)
+        self.dropout = nn.Dropout(ffn_dropout)
+
+    def forward(self, src: Tensor, src_mask: Tensor, tgt: Tensor, tgt_mask: Tensor):
+        tgt = self.norm1(tgt + self.dropout(self.tgt(q=tgt, k=tgt, v=tgt, mask=tgt_mask)))
+        tgt = self.norm2(tgt + self.dropout(self.src(q=tgt, k=src, v=src, mask=src_mask)))
+        tgt = self.norm3(tgt + self.dropout(self.ffn(tgt)))
+        return tgt
