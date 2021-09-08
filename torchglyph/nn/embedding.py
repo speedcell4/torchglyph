92a4ecf72eb284bc794cc747deaad216b99132a8
speedcell4 <speedcell4@gmail.com>
2021-09-08 23:03:19 +0900

    Feat: Add PosEmbedding and TriangularPosEmbedding

--- torchglyph/nn/embedding.py
+++ torchglyph/nn/embedding.py
@@ -1,13 +1,19 @@
+from typing import Union
+
+import torch
 from einops import rearrange
 from torch import nn, Tensor
 from torch.nn.utils.rnn import PackedSequence
-from torchrua import PackedMeta, PackedSequential
+from torchrua import PackedMeta, PackedSequential, batch_sizes_to_ptr
 
+from torchglyph.nn.init import bert_normal_
 from torchglyph.vocab import Vocab
 
 __all__ = [
     'TokenEmbedding',
     'CharLstmEmbedding',
+    'PosEmbedding',
+    'TriangularPosEmbedding',
 ]
 
 
@@ -31,9 +37,10 @@ class TokenEmbedding(nn.Embedding, metaclass=PackedMeta):
         self.weight.requires_grad = not freeze
 
     def extra_repr(self) -> str:
+        args = [super(TokenEmbedding, self).extra_repr()]
         if not self.weight.requires_grad:
-            return ', '.join([super(TokenEmbedding, self).extra_repr(), 'frozen'])
-        return super(TokenEmbedding, self).extra_repr()
+            args.append('frozen')
+        return ', '.join(args)
 
     def padding_mask(self, indices: Tensor) -> Tensor:
         return indices == (self.padding_idx if self.padding_idx is None else -100)
@@ -64,3 +71,54 @@ class CharLstmEmbedding(nn.Module):
         embedding = self.embedding(indices)
         _, (encoding, _) = self.rnn(embedding)
         return rearrange(encoding, '(l d) b x -> l b (d x)', d=self.num_directions)[-1]
+
+
+class PosEmbedding(nn.Module):
+    def __init__(self, embedding_dim: int, num_embeddings: int = 1024, freeze: bool = False, *,
+                 dtype: torch.dtype = torch.float32) -> None:
+        super(PosEmbedding, self).__init__()
+
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+
+        self.weight = nn.Parameter(self.obtain_weight(dtype=dtype), requires_grad=not freeze)
+
+    def extra_repr(self) -> str:
+        args = [
+            f'{self.num_embeddings}',
+            f'{self.embedding_dim}',
+        ]
+        if not self.weight.requires_grad:
+            args.append('frozen')
+        return ', '.join(args)
+
+    @torch.no_grad()
+    def obtain_weight(self, dtype: torch.dtype, **kwargs) -> Tensor:
+        return bert_normal_(torch.empty((self.num_embeddings, self.embedding_dim), dtype=dtype))
+
+    def forward(self, indices: Union[Tensor, PackedSequence], dim: int = -2) -> Union[Tensor, PackedSequence]:
+        if torch.is_tensor(indices):
+            return self.weight[:indices.size()[dim]]
+
+        batch_sizes = indices.batch_sizes.to(device=indices.data.device)
+        indices, _, _ = batch_sizes_to_ptr(batch_sizes=batch_sizes)
+        return torch.embedding(self.weight, indices)
+
+
+class TriangularPosEmbedding(PosEmbedding):
+    def __init__(self, embedding_dim: int, num_embeddings: int = 1024, freeze: bool = True, *,
+                 dtype: torch.dtype = torch.float32) -> None:
+        super(TriangularPosEmbedding, self).__init__(
+            embedding_dim=embedding_dim,
+            num_embeddings=num_embeddings,
+            freeze=freeze, dtype=dtype,
+        )
+
+    @torch.no_grad()
+    def obtain_weight(self, dtype: torch.dtype, **kwargs) -> Tensor:
+        token = torch.arange(self.num_embeddings, dtype=dtype)
+        feature = torch.arange(self.embedding_dim // 2, dtype=dtype)
+
+        index = token[:, None] / (10000. ** (feature[None, :] * 2. / self.embedding_dim))
+        embedding = torch.cat([torch.sin(index), torch.cos(index)], dim=-1)
+        return rearrange(embedding, 'n (s x) -> n (x s)', s=2)
