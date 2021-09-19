860cdc0c83d51be41e0ed657ca42d707cde1d67e
speedcell4 <speedcell4@gmail.com>
2021-09-19 12:41:47 +0900

    Refactor: Renaming padding.py

--- torchglyph/proc/padding.py
+++ torchglyph/proc/padding.py
@@ -1,61 +1,63 @@
+from abc import ABCMeta
 from typing import List, Any
 
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
-from torchrua import pad_sequence, pad_packed_sequence, pad_catted_sequence
+from torch.types import Device, Number
+from torchrua import pad_sequence, pad_packed_sequence, pad_catted_sequence, CattedSequence
 
-from torchglyph.types import Device, Number, CattedSequence
 from torchglyph.proc.abc import Proc
 
 __all__ = [
-    'PadProc',
-    'PadSequences', 'PadCattedSequence', 'PadPackedSequence',
+    'PaddingProc',
+    'PadSequences',
+    'PadCattedSequence',
+    'PadPackedSequence',
 ]
 
 
-class PadProc(Proc):
-    def __init__(self, batch_first: bool = True, padding_value: Number = 0, device: Device = None) -> None:
-        super(PadProc, self).__init__()
+class PaddingProc(Proc, metaclass=ABCMeta):
+    def __init__(self, batch_first: bool = True,
+                 padding_value: Number = 0, device: Device = None) -> None:
+        super(PaddingProc, self).__init__()
         self.batch_first = batch_first
         self.padding_value = padding_value
         self.device = device
 
     def extra_repr(self) -> str:
-        args = [
+        return ', '.join([
             f'batch_first={self.batch_first}',
             f'padding_value={self.padding_value}',
-        ]
-        if self.device is not None:
-            args.append(f'device={self.device}')
-        return ', '.join(args)
+            f'device={self.device}',
+        ])
 
-    def __call__(self, sequence: Any, **kwargs) -> Tensor:
+    def __call__(self, data: Any, **kwargs) -> Tensor:
         raise NotImplementedError
 
 
-class PadSequences(PadProc):
-    def __call__(self, sequences: List[Tensor], **kwargs) -> Tensor:
+class PadSequences(PaddingProc):
+    def __call__(self, data: List[Tensor], **kwargs) -> Tensor:
         return pad_sequence(
-            sequences=sequences, batch_first=self.batch_first,
+            sequences=data, batch_first=self.batch_first,
             padding_value=self.padding_value, device=self.device,
         )
 
 
-class PadPackedSequence(PadProc):
-    def __call__(self, sequence: PackedSequence, **kwargs) -> Tensor:
+class PadPackedSequence(PaddingProc):
+    def __call__(self, data: PackedSequence, **kwargs) -> Tensor:
         data, _ = pad_packed_sequence(
-            sequence=sequence,
+            sequence=data,
             batch_first=self.batch_first,
             padding_value=self.padding_value,
         )
         return data
 
 
-class PadCattedSequence(PadProc):
-    def __call__(self, sequence: CattedSequence, **kwargs) -> Tensor:
-        sequence, token_sizes = sequence
+class PadCattedSequence(PaddingProc):
+    def __call__(self, data: CattedSequence, **kwargs) -> Tensor:
+        data, token_sizes = data
         return pad_catted_sequence(
-            sequence=sequence, token_sizes=token_sizes,
+            sequence=data, token_sizes=token_sizes,
             batch_first=self.batch_first,
             padding_value=self.padding_value,
         )
