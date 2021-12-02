607edd29e90659b60564fdc4de1b403be8e54e80
speedcell4 <speedcell4@gmail.com>
2021-12-02 18:59:33 +0900

    Feat: Add Adam

--- torchglyph/optimizer.py
+++ torchglyph/optimizer.py
@@ -2,11 +2,16 @@ from typing import Iterable, Union, Type
 
 from torch import nn, optim
 
+__all__ = [
+    'Optimizers',
+    'SGD', 'Adam',
+]
+
 
 class SGD(optim.SGD):
     def __init__(self, lr: float = 1e-3, momentum: float = 0.9,
-                 dampening: float = 0., weight_decay: float = 1e-6,
-                 nesterov: bool = False, *, params: Iterable[nn.Parameter]) -> None:
+                 dampening: float = 0.0, weight_decay: float = 1e-6, nesterov: bool = False, *,
+                 params: Iterable[nn.Parameter]) -> None:
         super(SGD, self).__init__(
             params=params, lr=lr,
             momentum=momentum, dampening=dampening,
@@ -14,6 +19,17 @@ class SGD(optim.SGD):
         )
 
 
+class Adam(optim.AdamW):
+    def __init__(self, lr: float = 3e-4, beta1: float = 0.9, beta2: float = 0.98,
+                 weight_decay: float = 1e-8, amsgrad: bool = False, *,
+                 params: Iterable[nn.Parameter]) -> None:
+        super(Adam, self).__init__(
+            params=params, lr=lr, betas=(beta1, beta2),
+            weight_decay=weight_decay, amsgrad=amsgrad,
+        )
+
+
 Optimizers = Union[
     Type[SGD],
+    Type[Adam],
 ]
