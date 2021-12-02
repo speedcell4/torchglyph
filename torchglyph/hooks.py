4a6239a1fcf1d4cff517c66bd5a668651a22369e
speedcell4 <speedcell4@gmail.com>
2021-12-02 14:59:47 +0900

    Feat: Add summary

--- torchglyph/hooks.py
+++ torchglyph/hooks.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from typing import Tuple, Dict, Any
+from typing import Tuple, Dict, Any, List
 
 LOG_TXT = 'log.txt'
 ARGS_JSON = 'args.json'
@@ -24,11 +24,21 @@ def save_sota(*, out_dir: Path, **kwargs) -> None:
     return save_kwargs(name=SOTA_JSON, out_dir=out_dir, **kwargs)
 
 
-def fetch(out_dir: Path) -> Tuple[Dict[str, Any], Dict[str, Any]]:
-    with (out_dir / ARGS_JSON).open(mode='r', encoding='utf-8') as fp:
-        args = json.load(fp)
-
+def fetch(out_dir: Path, ignores: Tuple[str, ...]) -> Tuple[Dict[str, Any], Dict[str, Any]]:
     with (out_dir / SOTA_JSON).open(mode='r', encoding='utf-8') as fp:
         sota = json.load(fp)
+        sota = {key: value for key, value in sota.items() if key not in ignores}
 
-    return sota, {**args, 'path': out_dir / LOG_TXT}
+    with (out_dir / ARGS_JSON).open(mode='r', encoding='utf-8') as fp:
+        args = {**json.load(fp), 'path': out_dir / LOG_TXT}
+        args = {key: value for key, value in args.items() if key not in ignores}
+
+    return sota, args
+
+
+def summary(path: List[Path], keys: Tuple[str, ...], ignores: Tuple[str, ...] = ('device', 'seed', 'path')):
+    sota, args = zip(*[
+        fetch(out_dir=out_dir, ignores=ignores)
+        for p in path for out_dir in p.iterdir()
+        if out_dir.is_dir() and (out_dir / ARGS_JSON).exists() and (out_dir / SOTA_JSON).exists()
+    ])
