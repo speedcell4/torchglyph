cfe5865d9f8cb0b1326431f9d6b20f6bd26a96eb
speedcell4 <speedcell4@gmail.com>
2021-12-02 15:07:54 +0900

    Feat: Add load_checkpoint

--- torchglyph/hooks.py
+++ torchglyph/hooks.py
@@ -1,10 +1,18 @@
 import json
+from logging import getLogger
 from pathlib import Path
-from typing import Tuple, Dict, Any, List
+from typing import Any
+from typing import List, Tuple, Dict
+
+import torch
+from torch import nn
+
+logger = getLogger(__name__)
 
 LOG_TXT = 'log.txt'
 ARGS_JSON = 'args.json'
 SOTA_JSON = 'sota.json'
+CHECKPOINT_PT = 'checkpoint.pt'
 
 
 def save_kwargs(name: str, *, out_dir: Path, **kwargs) -> None:
@@ -22,6 +30,26 @@ def save_args(*, out_dir: Path, **kwargs) -> None:
 
 def save_sota(*, out_dir: Path, **kwargs) -> None:
     return save_kwargs(name=SOTA_JSON, out_dir=out_dir, **kwargs)
+
+
+def save_checkpoint(name: str = CHECKPOINT_PT, *, out_dir: Path, **kwargs) -> None:
+    logger.info(f'saving checkpoint to {out_dir / name}')
+    return torch.save({name: module.state_dict() for name, module in kwargs.items()}, f=out_dir / name)
+
+
+def load_checkpoint(name: str = CHECKPOINT_PT, strict: bool = True, *, out_dir: Path, **kwargs) -> None:
+    state_dict = torch.load(out_dir / name, map_location=torch.device('cpu'))
+
+    logger.info(f'loading checkpoint from {out_dir / name}')
+    for name, module in kwargs.items():  # type: str, nn.Module
+        missing_keys, unexpected_keys = module.load_state_dict(state_dict=state_dict[name], strict=strict)
+
+        if not strict:
+            for missing_key in missing_keys:
+                logger.warning(f'{missing_key} is missing')
+
+            for unexpected_key in unexpected_keys:
+                logger.warning(f'{unexpected_key} is unexpected')
 
 
 def fetch(out_dir: Path, ignores: Tuple[str, ...]) -> Tuple[Dict[str, Any], Dict[str, Any]]:
