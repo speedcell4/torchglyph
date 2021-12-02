bb462a5cfd2417e9f830383ca9d0981cd9d25c80
speedcell4 <speedcell4@gmail.com>
2021-12-02 14:45:55 +0900

    Feat: Add fetch

--- torchglyph/hooks.py
+++ torchglyph/hooks.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
+from typing import Tuple, Dict, Any
 
+LOG_TXT = 'log.txt'
 ARGS_JSON = 'args.json'
 SOTA_JSON = 'sota.json'
 
@@ -14,9 +16,19 @@ def save_kwargs(name: str, *, out_dir: Path, **kwargs) -> None:
         json.dump(kwargs, fp=fp, indent=2, sort_keys=True)
 
 
-def save_args(name: str = ARGS_JSON, *, out_dir: Path, **kwargs) -> None:
-    return save_kwargs(name=name, out_dir=out_dir, **kwargs)
+def save_args(*, out_dir: Path, **kwargs) -> None:
+    return save_kwargs(name=ARGS_JSON, out_dir=out_dir, **kwargs)
 
 
-def save_sota(name: str = SOTA_JSON, *, out_dir: Path, **kwargs) -> None:
-    return save_kwargs(name=name, out_dir=out_dir, **kwargs)
+def save_sota(*, out_dir: Path, **kwargs) -> None:
+    return save_kwargs(name=SOTA_JSON, out_dir=out_dir, **kwargs)
+
+
+def fetch(out_dir: Path) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    with (out_dir / ARGS_JSON).open(mode='r', encoding='utf-8') as fp:
+        args = json.load(fp)
+
+    with (out_dir / SOTA_JSON).open(mode='r', encoding='utf-8') as fp:
+        sota = json.load(fp)
+
+    return sota, {**args, 'path': out_dir / LOG_TXT}
