a941c50e6390ca974a82e8ba597d8ed4fe61557f
speedcell4 <speedcell4@gmail.com>
2021-12-22 15:53:28 +0900

    Refactor: Update CosineLinear

--- torchglyph/nn/linear.py
+++ torchglyph/nn/linear.py
@@ -1,10 +1,13 @@
 import torch
+from torch import Tensor
 from torch import nn
+from torch.nn import functional as F
+from torch.nn import init
 
 from torchglyph.nn import init
 
 __all__ = [
-    'Linear',
+    'Linear', 'CosineLinear',
 ]
 
 
@@ -21,3 +24,10 @@ class Linear(nn.Linear):
         init.kaiming_uniform_(self.weight, fan=self.in_features, a=5 ** 0.5, nonlinearity='leaky_relu')
         if self.bias is not None:
             init.constant_(self.bias, 0.)
+
+
+class CosineLinear(Linear):
+    def forward(self, tensor: Tensor) -> Tensor:
+        tensor = F.normalize(tensor, p=2, dim=-1)
+        weight = F.normalize(self.weight, p=2, dim=-1)
+        return F.linear(tensor, weight=weight, bias=self.bias)
