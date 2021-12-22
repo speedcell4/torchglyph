f28bacc80e2b3d0fd692381b2d311656ad325ba6
speedcell4 <speedcell4@gmail.com>
2021-12-22 15:51:52 +0900

    Refactor: Update Linear

--- torchglyph/nn/linear.py
+++ torchglyph/nn/linear.py
@@ -1,21 +1,17 @@
 import torch
-from torch import Tensor
 from torch import nn
-from torch.nn import functional as F
-from torch.nn import init
 
 from torchglyph.nn import init
 
 __all__ = [
-    'Linear', 'ConjugatedLinear',
-    'Projector', 'ConjugatedProjector',
-    'CosProjector', 'CosConjugatedProjector',
+    'Linear',
 ]
 
 
 class Linear(nn.Linear):
     def __init__(self, bias: bool = True, *,
-                 in_features: int, out_features: int, dtype: torch.dtype = torch.float32) -> None:
+                 in_features: int, out_features: int,
+                 dtype: torch.dtype = torch.float32) -> None:
         super(Linear, self).__init__(
             in_features=in_features, out_features=out_features,
             bias=bias, dtype=dtype,
@@ -25,111 +21,3 @@ class Linear(nn.Linear):
         init.kaiming_uniform_(self.weight, fan=self.in_features, a=5 ** 0.5, nonlinearity='leaky_relu')
         if self.bias is not None:
             init.constant_(self.bias, 0.)
-
-
-class Projector(nn.Linear):
-    def __init__(self, bias: bool = False, *,
-                 in_features: int, out_features: int, dtype: torch.dtype = torch.float32) -> None:
-        super(Projector, self).__init__(
-            in_features=in_features, out_features=out_features,
-            bias=bias, dtype=dtype,
-        )
-
-    def reset_parameters(self) -> None:
-        init.kaiming_uniform_(self.weight, self.in_features, nonlinearity='relu')
-        if self.bias is not None:
-            init.constant_(self.bias, 0.)
-
-
-class CosProjector(Projector):
-    def __init__(self, bias: bool = False, tau: float = 0.1, *,
-                 in_features: int, out_features: int, dtype: torch.dtype = torch.float32) -> None:
-        super(CosProjector, self).__init__(
-            in_features=in_features, out_features=out_features,
-            bias=bias, dtype=dtype,
-        )
-        self.tau = tau
-
-    def extra_repr(self) -> str:
-        return ', '.join([
-            super(CosProjector, self).extra_repr(),
-            f'tau={self.tau}',
-        ])
-
-    def forward(self, tensor: Tensor) -> Tensor:
-        tensor = F.normalize(tensor, p=2, dim=-1)
-        weight = F.normalize(self.weight, p=2, dim=-1)
-        out = (weight @ tensor) / self.tau
-        if self.bias is not None:
-            out = out + self.bias
-        return out
-
-
-class ConjugatedLinear(nn.Module):
-    def __init__(self, bias: bool = True, *,
-                 in_features: int, num_conjugates: int, out_features: int,
-                 dtype: torch.dtype = torch.float32) -> None:
-        super(ConjugatedLinear, self).__init__()
-
-        self.in_features = in_features
-        self.num_conjugates = num_conjugates
-        self.out_features = out_features
-
-        self.weight = nn.Parameter(torch.empty((num_conjugates, out_features, in_features), dtype=dtype))
-        if bias:
-            self.bias = nn.Parameter(torch.empty((num_conjugates, out_features), dtype=dtype))
-        else:
-            self.register_parameter('bias', None)
-
-        self.reset_parameters()
-
-    def reset_parameters(self) -> None:
-        init.kaiming_uniform_(self.weight, fan=self.in_features, a=5 ** 0.5, nonlinearity='leaky_relu')
-        if self.bias is not None:
-            init.constant_(self.bias, 0.)
-
-    def extra_repr(self) -> str:
-        return ', '.join([
-            f'in_features={self.in_features}',
-            f'num_conjugates={self.num_conjugates}',
-            f'out_features={self.out_features}',
-            f'bias={self.bias is not None}',
-        ])
-
-    def forward(self, tensor: Tensor) -> Tensor:
-        out = (self.weight @ tensor[..., None])[..., 0]
-        if self.bias is not None:
-            out = out + self.bias
-        return out
-
-
-class ConjugatedProjector(ConjugatedLinear):
-    def reset_parameters(self) -> None:
-        init.kaiming_uniform_(self.weight, fan=self.in_features, nonlinearity='relu')
-        if self.bias is not None:
-            init.constant_(self.bias, 0.)
-
-
-class CosConjugatedProjector(ConjugatedProjector):
-    def __init__(self, bias: bool = True, tau: float = 0.1, *,
-                 in_features: int, num_conjugates: int, out_features: int,
-                 dtype: torch.dtype = torch.float32) -> None:
-        super(CosConjugatedProjector, self).__init__(
-            in_features=in_features, out_features=out_features,
-            num_conjugates=num_conjugates, bias=bias, dtype=dtype,
-        )
-        self.tau = tau
-
-    def extra_repr(self) -> str:
-        return ', '.join([
-            super(CosConjugatedProjector, self).extra_repr(),
-            f'tau={self.tau}',
-        ])
-
-    def forward(self, tensor: Tensor) -> Tensor:
-        tensor = F.normalize(tensor, p=2, dim=-1)
-        weight = F.normalize(self.weight, p=2, dim=-1)
-        out = (weight @ tensor[..., None])[..., 0] / self.tau
-        if self.bias is not None:
-            out = out + self.bias
-        return out
