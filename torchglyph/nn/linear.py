20cc23306449512a089cf323290fa990359d863a
speedcell4 <speedcell4@gmail.com>
2021-12-22 15:59:47 +0900

    Refactor: Update ConjugatedLinear

--- torchglyph/nn/linear.py
+++ torchglyph/nn/linear.py
@@ -7,7 +7,7 @@ from torch.nn import init
 from torchglyph.nn import init
 
 __all__ = [
-    'Linear', 'CosineLinear',
+    'Linear', 'CosineLinear', 'ConjugatedLinear',
 ]
 
 
@@ -31,3 +31,41 @@ class CosineLinear(Linear):
         tensor = F.normalize(tensor, p=2, dim=-1)
         weight = F.normalize(self.weight, p=2, dim=-1)
         return F.linear(tensor, weight=weight, bias=self.bias)
+
+
+class ConjugatedLinear(nn.Module):
+    def __init__(self, bias: bool = True, *,
+                 num_conjugates: int, in_features: int, out_features: int,
+                 dtype: torch.dtype = torch.float32) -> None:
+        super(ConjugatedLinear, self).__init__()
+
+        self.weight = nn.Parameter(torch.empty((num_conjugates, out_features, in_features), dtype=dtype))
+        if bias:
+            self.bias = nn.Parameter(torch.empty((num_conjugates, out_features), dtype=dtype))
+        else:
+            self.register_parameter('bias', None)
+
+        self.num_conjugates = num_conjugates
+        self.in_features = in_features
+        self.out_features = out_features
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        init.kaiming_uniform_(self.weight, fan=self.in_features, a=5 ** 0.5, nonlinearity='leaky_relu')
+        if self.bias is not None:
+            init.constant_(self.bias, 0.)
+
+    def extra_repr(self) -> str:
+        return ', '.join([
+            f'in_features={self.in_features}',
+            f'num_conjugates={self.num_conjugates}',
+            f'out_features={self.out_features}',
+            f'bias={self.bias is not None}',
+        ])
+
+    def forward(self, tensor: Tensor) -> Tensor:
+        out = (self.weight @ tensor[..., None])[..., 0]
+        if self.bias is not None:
+            out = out + self.bias
+        return out
