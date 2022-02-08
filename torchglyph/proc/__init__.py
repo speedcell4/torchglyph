de4ca328f8bcff571b5be5513f12f92ffafece03
speedcell4 <speedcell4@gmail.com>
2022-02-08 23:05:28 +0900

    Refactor: Remove ctx.py

--- torchglyph/proc/__init__.py
+++ torchglyph/proc/__init__.py
@@ -2,7 +2,6 @@ from torchglyph.proc.abc import *
 from torchglyph.proc.basic import *
 from torchglyph.proc.catting import *
 from torchglyph.proc.collating import *
-from torchglyph.proc.ctx import *
 from torchglyph.proc.mlm import *
 from torchglyph.proc.packing import *
 from torchglyph.proc.padding import *
