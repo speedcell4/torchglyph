6f4b01bebc9006762e2ef672481f11f4752ba7e8
speedcell4 <speedcell4@gmail.com>
2022-05-14 20:29:49 +0900

    Feat: Add SizedBatchSampler

--- tests/test_datasets/test_named_entity_recognition.py
+++ tests/test_datasets/test_named_entity_recognition.py
@@ -6,9 +6,9 @@ from torchglyph.datasets.named_entity_recognition import CoNLL2003
 
 def test_conll2003():
     train, dev, test = CoNLL2003.new(batch_size=32, device=torch.device('cpu'))
-    assert len(train.dataset) == 14987
-    assert len(dev.dataset) == 3466
-    assert len(test.dataset) == 3684
+    assert len(train.data_source) == 14987
+    assert len(dev.data_source) == 3466
+    assert len(test.data_source) == 3684
 
     for batch in train:
         assert isinstance(batch.word, PackedSequence)
--- torchglyph/sampler.py
+++ torchglyph/sampler.py
@@ -2,17 +2,16 @@ from typing import Iterator
 from typing import List
 
 import torch
-from torch.utils.data import BatchSampler as TorchBatchSampler, Sampler
-
-from torchglyph.dataset import DatasetABC
+from torch.utils.data import BatchSampler, Sampler
 
 __all__ = [
-    'BatchSampler',
+    'SortishSampler',
+    'SizedBatchSampler',
 ]
 
 
 class SortishSampler(Sampler[int]):
-    def __init__(self, data_source: DatasetABC, chunk_size: int) -> None:
+    def __init__(self, data_source, chunk_size: int) -> None:
         super(SortishSampler, self).__init__(data_source=data_source)
 
         self.chunk_size = chunk_size
@@ -34,38 +33,25 @@ class SortishSampler(Sampler[int]):
             yield from chunk[indices].detach().tolist()
 
 
-class BatchSampler(TorchBatchSampler):
-    def __init__(self, dataset, sampler: Sampler[int], batch_size: int, drop_last: bool) -> None:
-        super(BatchSampler, self).__init__(sampler, batch_size, drop_last)
+class SizedBatchSampler(BatchSampler):
+    def __init__(self, data_source, sampler: Sampler[int], batch_size: int, drop_last: bool) -> None:
+        super(SizedBatchSampler, self).__init__(sampler=sampler, batch_size=batch_size, drop_last=drop_last)
 
-        self.dataset = dataset
-        self.reset_indices()
-
-    def reset_indices(self) -> None:
-        self.batch_indices = []
-
-        batch_size = 0
-        batch_indices = []
-        for index in self.sampler:
-            data_size = self.dataset.get_size(self.dataset[index])
-
-            if (data_size + batch_size) > self.batch_size:
-                if batch_size > 0:
-                    self.batch_indices.append(batch_indices)
-                    batch_size = 0
-                    batch_indices = []
-                else:
-                    continue
-
-            batch_indices.append(index)
-            batch_size += data_size
-
-        if not self.drop_last and batch_size > 0:
-            self.batch_indices.append(batch_indices)
+        self.sizes = data_source.sizes
 
     def __len__(self) -> int:
-        return len(self.batch_indices)
+        return len(self.sizes)
 
     def __iter__(self) -> Iterator[List[int]]:
-        yield from self.batch_indices
-        self.reset_indices()
+        batch, batch_size = [], 0
+
+        for index in self.sampler:
+            if (self.sizes[index] + batch_size) > self.batch_size:
+                yield batch
+                batch, batch_size = [], 0
+
+            batch.append(index)
+            batch_size += self.sizes[index]
+
+        if not self.drop_last and batch_size > 0:
+            yield batch
