8615abb4dc53dffb87e1cbe205dc2503c11d6680
speedcell4 <speedcell4@gmail.com>
2022-07-12 23:05:55 +0900

    Fix: Resolve CattedNumListPipe.inv

--- torchglyph/pipe/catting.py
+++ torchglyph/pipe/catting.py
@@ -2,10 +2,10 @@ from typing import List, Tuple, Optional
 
 import torch
 from torch.types import Device, Number
-from torchglyph.proc.abc import Lift
 from torchrua import CattedSequence
 
 from torchglyph.pipe.abc import Pipe
+from torchglyph.proc.abc import Lift
 from torchglyph.proc.catting import CatSequence
 from torchglyph.proc.collating import ToTensor
 from torchglyph.proc.vocab import CountSequence, BuildVocab, ToIndex, StatsVocab
@@ -26,17 +26,13 @@ class CattedNumListPipe(Pipe):
         )
 
     def inv(self, sequence: CattedSequence) -> List[List[Number]]:
-        data, token_sizes = sequence
+        data = sequence.data.detach()
+        token_sizes = sequence.token_sizes.detach().cpu().tolist()
 
-        data = data.detach().cpu().tolist()
-        token_sizes = token_sizes.detach().cpu().tolist()
-
-        out, start, end = [], 0, 0
-        for token_size in token_sizes:
-            start, end = end, end + token_size
-            out.append(data[start:end])
-
-        return data
+        return [
+            tensor.detach().tolist()
+            for tensor in torch.split(data, token_sizes, dim=0)
+        ]
 
 
 class CattedStrListPipe(CattedNumListPipe):
