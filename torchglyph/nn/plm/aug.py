1c0ffba2beefbe5ea55d2e065819e35510ee1e20
speedcell4 <speedcell4@gmail.com>
2022-07-16 18:56:28 +0900

    Feat: Add poisson_span_mask

--- torchglyph/nn/plm/aug.py
+++ torchglyph/nn/plm/aug.py
@@ -1,15 +1,22 @@
+from typing import Optional
+
 import torch
 from torch import Tensor
-
+from torch.distributions import Poisson
 from transformers import PreTrainedTokenizer
 
 
-def uniform_token_mask(sequence: Tensor, tokenizer: PreTrainedTokenizer, p: float) -> Tensor:
+def is_special(sequence: Tensor, duration: Optional[Tensor], tokenizer: PreTrainedTokenizer) -> Tensor:
     special_tokens = torch.tensor(tokenizer.all_special_ids, dtype=sequence.dtype, device=sequence.device)
-    is_special = (sequence[..., None] == special_tokens).any(dim=-1)
+    mask = (sequence[..., None] == special_tokens).any(dim=-1)
+    if duration is not None:
+        mask = torch.segment_reduce(mask.float(), reduce='max', lengths=duration).bool()
+    return mask
 
+
+def uniform_token_mask(sequence: Tensor, tokenizer: PreTrainedTokenizer, p: float) -> Tensor:
     mask = torch.rand_like(sequence, dtype=torch.float32) < p
-    mask[is_special] = False
+    mask[is_special(sequence=sequence, duration=None, tokenizer=tokenizer)] = False
 
     sequence = sequence.clone()
     sequence[mask] = tokenizer.mask_token_id
@@ -18,12 +25,19 @@ def uniform_token_mask(sequence: Tensor, tokenizer: PreTrainedTokenizer, p: floa
 
 
 def uniform_span_mask(sequence: Tensor, duration: Tensor, tokenizer: PreTrainedTokenizer, p: float) -> Tensor:
-    special_tokens = torch.tensor(tokenizer.all_special_ids, dtype=sequence.dtype, device=sequence.device)
-    special_tokens = (sequence[..., None] == special_tokens).any(dim=-1)
-    is_special = torch.segment_reduce(special_tokens.float(), reduce='max', lengths=duration).bool()
-
     mask = torch.rand_like(duration, dtype=torch.float32) < p
-    mask[is_special] = False
+    mask[is_special(sequence=sequence, duration=duration, tokenizer=tokenizer)] = False
+    mask = torch.repeat_interleave(mask.float(), repeats=duration, dim=0).bool()
+
+    sequence = sequence.clone()
+    sequence[mask] = tokenizer.mask_token_id
+
+    return sequence
+
+
+def poisson_span_mask(sequence: Tensor, duration: Tensor, tokenizer: PreTrainedTokenizer, rate: float) -> Tensor:
+    mask = torch.rand_like(duration, dtype=torch.float32) < Poisson(rate=rate).log_prob(duration).exp()
+    mask[is_special(sequence=sequence, duration=duration, tokenizer=tokenizer)] = False
     mask = torch.repeat_interleave(mask.float(), repeats=duration, dim=0).bool()
 
     sequence = sequence.clone()
