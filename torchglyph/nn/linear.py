2b59fc145c074f7e0576b6ac7eeb9825bef1b8ee
speedcell4 <speedcell4@gmail.com>
2022-11-06 01:32:54 +0900

    Feat: Add Linear with annotations

--- torchglyph/nn/linear.py
+++ torchglyph/nn/linear.py
@@ -1,13 +1,41 @@
 import torch
 from torch import Tensor
 from torch import nn
-from torch.nn.init import uniform_, zeros_
+from torchrua import RuaMeta
 
 from torchglyph.functional import conjugated_linear
-from torchglyph.nn.init import kaiming_uniform_, orthogonal_, bert_normal_
+from torchglyph.nn.init import kaiming_uniform_, orthogonal_, bert_normal_, zeros_
 
 
-class ConjugatedLinear(nn.Module):
+class Linear(nn.Linear, metaclass=RuaMeta):
+    def __init__(self, bias: bool = True, *, in_features: int, out_features: int) -> None:
+        super(Linear, self).__init__(in_features=in_features, out_features=out_features, bias=bias)
+
+    def reset_parameters(self) -> None:
+        kaiming_uniform_(self.weight, fan=self.in_features, a=5 ** 0.5)
+        if self.bias is not None:
+            zeros_(self.bias)
+
+
+class KaimingLinear(Linear):
+    pass
+
+
+class OrthogonalLinear(Linear):
+    def reset_parameters(self) -> None:
+        orthogonal_(self.weight)
+        if self.bias is not None:
+            zeros_(self.bias)
+
+
+class BertLinear(Linear):
+    def reset_parameters(self) -> None:
+        bert_normal_(self.weight)
+        if self.bias is not None:
+            zeros_(self.bias)
+
+
+class ConjugatedLinear(nn.Module, metaclass=RuaMeta):
     def __init__(self, num_conjugates: int = 1, bias: bool = True, *, in_features: int, out_features: int) -> None:
         super(ConjugatedLinear, self).__init__()
 
@@ -31,8 +59,7 @@ class ConjugatedLinear(nn.Module):
     def reset_parameters(self) -> None:
         kaiming_uniform_(self.weight, fan=self.in_features, a=5 ** 0.5)
         if self.bias is not None:
-            bound = self.in_features ** -0.5
-            uniform_(self.bias, -bound, +bound)
+            zeros_(self.bias)
 
     def forward(self, tensor: Tensor) -> Tensor:
         return conjugated_linear(tensor, weight=self.weight, bias=self.bias)
