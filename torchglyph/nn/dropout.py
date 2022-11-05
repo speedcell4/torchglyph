30381d814ffc02c50bd86cef965188ac460432d7
speedcell4 <speedcell4@gmail.com>
2022-11-06 01:36:31 +0900

    Feat: Update VarDropout with annotations

--- torchglyph/nn/dropout.py
+++ torchglyph/nn/dropout.py
@@ -3,25 +3,27 @@ from typing import Union
 import torch
 from torch import nn
 from torch.nn.utils.rnn import PackedSequence
+from torchrua import CattedSequence, RuaMeta
+from torchrua.utils import sequence_ptr, sequence_shape
 
-from torchrua import CattedSequence, RuaMeta, sequence_shape, sequence_ptr
+Sequence = Union[CattedSequence, PackedSequence]
 
 
 class Dropout(nn.Dropout, metaclass=RuaMeta):
-    def __init__(self, p: float) -> None:
+    def __init__(self, p: float = 0.0) -> None:
         super(Dropout, self).__init__(p=p, inplace=False)
 
 
 class VarDropout(nn.Dropout):
-    def __init__(self, p: float) -> None:
+    def __init__(self, p: float = 0.0) -> None:
         super(VarDropout, self).__init__(p=p, inplace=True)
 
-    def forward(self, sequence: Union[CattedSequence, PackedSequence]) -> Union[CattedSequence, PackedSequence]:
+    def forward(self, sequence: Sequence) -> Sequence:
         if not self.training:
             return sequence
 
         _, batch_size, _ = sequence_shape(sequence=sequence)
-        batch_ptr, token_ptr = sequence_ptr(sequence=sequence)
+        batch_ptr, _ = sequence_ptr(sequence=sequence)
 
         mask = torch.ones(
             (batch_size, *sequence.data.size()[1:]),
