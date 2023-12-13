8b66a20ec32716df0795690fbd64df9b3fca8390
speedcell4 <speedcell4@gmail.com>
2023-12-13 23:00:22 +0900

    Feat: Update activation.py

--- torchglyph/nn/activation.py
+++ torchglyph/nn/activation.py
@@ -3,6 +3,11 @@ from typing import Type, Union
 from torch import nn
 
 
+class Identity(nn.Identity):
+    def __init__(self) -> None:
+        super(Identity, self).__init__()
+
+
 class ReLU(nn.ReLU):
     def __init__(self) -> None:
         super(ReLU, self).__init__()
@@ -23,9 +28,16 @@ class ELU(nn.ELU):
         super(ELU, self).__init__()
 
 
+class Tanh(nn.Tanh):
+    def __init__(self) -> None:
+        super(Tanh, self).__init__()
+
+
 Activations = Union[
+    Type[Identity],
     Type[ReLU],
     Type[GELU],
     Type[SiLU],
     Type[ELU],
+    Type[Tanh],
 ]
