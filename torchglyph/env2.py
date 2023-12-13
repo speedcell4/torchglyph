3f54baf5db696ecee62129cc8b57a9be23e3eb04
speedcell4 <speedcell4@gmail.com>
2023-12-14 01:12:33 +0900

    Feat: Add init_dir

--- torchglyph/env2.py
+++ torchglyph/env2.py
@@ -1,4 +1,7 @@
+import socket
+import warnings
 from contextlib import contextmanager
+from datetime import datetime
 from pathlib import Path
 
 from datasets.config import DATASETDICT_JSON_FILENAME
@@ -6,6 +9,7 @@ from datasets.fingerprint import Hasher
 from filelock import FileLock
 
 from torchglyph import DEBUG
+from torchglyph.serde import save_args
 
 
 @contextmanager
@@ -30,7 +34,7 @@ def get_cache(out_dir: Path, **kwargs) -> Path:
     out_dir.mkdir(parents=True, exist_ok=True)
 
     uuid1 = get_hash(**kwargs)
-    uuid2 = get_hash(**kwargs, __torchglyph=DEBUG)
+    uuid2 = get_hash(**kwargs, __debug=DEBUG)
 
     if DEBUG and not (out_dir / uuid1).exists():
         return out_dir / uuid2
@@ -43,3 +47,18 @@ def all_exists(out_dir: Path, *names: str) -> bool:
         (out_dir / name).exists()
         for name in (DATASETDICT_JSON_FILENAME, *names)
     )
+
+
+def init_dir(study: str, *, project_out_dir: Path, **kwargs) -> Path:
+    with lock_dir(out_dir=project_out_dir / study):
+        uuid = get_hash(__datetime=datetime.now() if DEBUG or study == 'demo' else None, **kwargs)
+        out_dir = project_out_dir / study / uuid
+
+        if out_dir.exists():
+            warnings.warn('duplicated experiment')
+            exit()
+
+        out_dir.mkdir(parents=True, exist_ok=False)
+        save_args(out_dir=out_dir, hostname=socket.gethostname(), **kwargs)
+
+    return out_dir
