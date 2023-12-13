b60d08902db9fb6c25814c0599a95799c8e07ed7
speedcell4 <speedcell4@gmail.com>
2023-12-13 23:26:25 +0900

    Feat: Update scheduler.py

--- torchglyph/scheduler.py
+++ torchglyph/scheduler.py
@@ -3,92 +3,61 @@ from typing import Type, Union
 
 from torch.optim import Optimizer, lr_scheduler
 
+from torchglyph import DEBUG
+
 logger = getLogger(__name__)
 
 
 class LambdaLR(lr_scheduler.LambdaLR):
+    def __init__(self, num_warmup_steps: int = 20 if DEBUG else 4000,
+                 num_training_steps: int = 20 if DEBUG else 200000, *,
+                 optimizer: Optimizer, last_epoch: int = -1, **kwargs) -> None:
+        self.num_warmup_steps = num_warmup_steps
+        self.num_training_steps = num_training_steps
+
+        super(LambdaLR, self).__init__(
+            optimizer=optimizer, last_epoch=last_epoch,
+            lr_lambda=self.lr_lambda,
+        )
+
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}({self.extra_repr()})'
 
     def extra_repr(self) -> str:
-        return ''
+        return ', '.join([
+            f'num_warmup_steps={self.num_warmup_steps}',
+            f'num_training_steps={self.num_training_steps}',
+        ])
 
-    def step(self, epoch: int = None) -> None:
-        super(LambdaLR, self).step(epoch=epoch)
-
-    def report_lr(self) -> None:
-        for group, lr in enumerate(self.get_last_lr()):
-            logger.info(f'group {group} | lr => {lr:.10f}')
+    def lr_lambda(self, step: int) -> float:
+        raise NotImplementedError
 
 
 class ConstantScheduler(LambdaLR):
-    def __init__(self, num_training_steps: int = 20_0000, num_warmup_steps: int = 5000, *,
-                 optimizer: Optimizer, last_epoch: int = -1, **kwargs) -> None:
-        self.num_warmup_steps = num_warmup_steps
-        self.num_training_steps = num_training_steps
+    def lr_lambda(self, step: int) -> float:
+        if step < self.num_warmup_steps:
+            return float(step / max(1.0, self.num_warmup_steps))
 
-        def lr_lambda(current_step: int) -> float:
-            if current_step < num_warmup_steps:
-                return float(current_step / max(1, num_warmup_steps))
-            return 1.0
-
-        super(ConstantScheduler, self).__init__(
-            optimizer=optimizer, lr_lambda=lr_lambda, last_epoch=last_epoch,
-
-        )
-
-    def extra_repr(self) -> str:
-        return ', '.join([
-            f'num_warmup_steps={self.num_warmup_steps}',
-            f'num_training_steps={self.num_training_steps}',
-        ])
+        return 1.0
 
 
 class LinearScheduler(LambdaLR):
-    def __init__(self, num_training_steps: int = 20_0000, num_warmup_steps: int = 5000, *,
-                 optimizer: Optimizer, last_epoch: int = -1, **kwargs) -> None:
-        self.num_warmup_steps = num_warmup_steps
-        self.num_training_steps = num_training_steps
+    def lr_lambda(self, step: int) -> float:
+        if step < self.num_warmup_steps:
+            return float(step / max(1.0, self.num_warmup_steps))
 
-        def lr_lambda(current_step: int) -> float:
-            if current_step < num_warmup_steps:
-                return float(current_step / max(1, num_warmup_steps))
-            return max(0., (num_training_steps - current_step) / max(1, num_training_steps - num_warmup_steps))
-
-        super(LinearScheduler, self).__init__(
-            optimizer=optimizer, lr_lambda=lr_lambda, last_epoch=last_epoch,
-        )
-
-    def extra_repr(self) -> str:
-        return ', '.join([
-            f'num_warmup_steps={self.num_warmup_steps}',
-            f'num_training_steps={self.num_training_steps}',
-        ])
+        return max(0., self.num_training_steps - step) / max(1.0, self.num_training_steps - self.num_warmup_steps)
 
 
 class InverseSquareRootScheduler(LambdaLR):
-    def __init__(self, num_training_steps: int = 20_0000, num_warmup_steps: int = 5000, *,
-                 optimizer: Optimizer, last_epoch: int = -1, **kwargs) -> None:
-        self.num_warmup_steps = num_warmup_steps
-        self.num_training_steps = num_training_steps
+    def lr_lambda(self, step: int) -> float:
+        if step < self.num_warmup_steps:
+            return float(step / max(1.0, self.num_warmup_steps))
 
-        def lr_lambda(current_step: int) -> float:
-            if current_step < num_warmup_steps:
-                return float(current_step / max(1, num_warmup_steps))
-            return max(0., (num_warmup_steps / max(1, current_step)) ** 0.5)
-
-        super(InverseSquareRootScheduler, self).__init__(
-            optimizer=optimizer, lr_lambda=lr_lambda, last_epoch=last_epoch,
-        )
-
-    def extra_repr(self) -> str:
-        return ', '.join([
-            f'num_warmup_steps={self.num_warmup_steps}',
-            f'num_training_steps={self.num_training_steps}',
-        ])
+        return max(0., (self.num_warmup_steps / step) ** 0.5)
 
 
-Schedulers = Union[
+Scheduler = Union[
     Type[ConstantScheduler],
     Type[LinearScheduler],
     Type[InverseSquareRootScheduler],
