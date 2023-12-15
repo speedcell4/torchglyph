247a35d737a9bfc8046fca013b44fec2a8a788bf
speedcell4 <speedcell4@gmail.com>
2023-12-15 21:12:12 +0900

    Feat: Add augmentation.py

---
+++ torchglyph/nn/augmentation.py
@@ -0,0 +1,45 @@
+from typing import Tuple, Union
+
+import torch
+from torch import nn
+from torchrua import C, D, P
+
+
+class Aug(nn.Module):
+    def __init__(self, p: float = 0.1, *, mask_token_id: int, special_token_ids: Tuple[int, ...]) -> None:
+        super(Aug, self).__init__()
+
+        self.p = p
+        self.mask_token_id = mask_token_id
+
+        special_token_ids = torch.tensor(special_token_ids, dtype=torch.long)
+        self.register_buffer('special_token_ids', special_token_ids)
+
+        boundaries = torch.tensor([p], dtype=torch.float32)
+        self.register_buffer('boundaries', boundaries)
+
+    def extra_repr(self) -> str:
+        return f', '.join([
+            f'p={self.p}',
+            f'mask={self.mask_token_id}',
+            f'specials={self.special_token_ids.detach().cpu().tolist()}',
+        ])
+
+    def forward(self, sequence: Union[C, D, P], **kwargs) -> Union[C, D, P]:
+        raise NotImplementedError
+
+
+class TokenAug(Aug):
+    def forward(self, sequence: Union[C, D, P], **kwargs) -> Union[C, D, P]:
+        if not self.training:
+            return sequence
+
+        data = sequence.data.clone()
+
+        bucket = torch.rand_like(data, dtype=torch.float32)
+        bucket = torch.bucketize(bucket, boundaries=self.boundaries)
+        bucket[(data[..., None] == self.special_token_ids).any(dim=-1)] = -1
+
+        data[bucket == 0] = self.mask_token_id
+
+        return sequence._replace(data=data)
