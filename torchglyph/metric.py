4f133cb9cba4519a8ddea1f8adeade1f8b533e84
speedcell4 <3585459+speedcell4@users.noreply.github.com>
2024-07-10 01:03:26 +0900

    Feat: Add HashMetric

--- torchglyph/metric.py
+++ torchglyph/metric.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import torch
 from torch import Tensor
 from torchmetrics import MaxMetric, MeanMetric, MetricCollection, MinMetric
 from torchrua import C, D, P
@@ -41,11 +42,29 @@ class SeqMetric(MetricCollection):
         self['max'].update(token_sizes)
 
 
-class AccuracyMetric(MeanMetric):
-    def update(self, argmax: Tensor, target: Tensor, ignore_index: int = -100):
-        if ignore_index is not None:
-            mask = target != ignore_index
-            argmax = argmax[mask]
-            target = target[mask]
+class HashMetric(MetricCollection):
+    def __init__(self) -> None:
+        super(HashMetric, self).__init__({
+            'loss': MeanMetric(),
+            'pos': MeanMetric(),
+            'neg': MeanMetric(),
+            'unique': MeanMetric(),
+        })
 
-        return super(AccuracyMetric, self).update((argmax == target) * 100.)
+    def update_loss(self, loss: Tensor) -> None:
+        self['loss'].update(loss)
+
+    def update(self, x: Tensor, y: Tensor, t1: Tensor, t2: Tensor) -> None:
+        pos = t1[:, None] == t2[None, :]
+        neg = t1[:, None] != t2[None, :]
+
+        self['pos'].update((x[:, None] == y[None, :])[pos].float())
+        self['neg'].update((x[:, None] != y[None, :])[neg].float())
+
+        x, *_ = torch.unique(x, dim=0).size()
+        y, *_ = torch.unique(y, dim=0).size()
+        t1, *_ = torch.unique(t1, dim=0).size()
+        t2, *_ = torch.unique(t2, dim=0).size()
+
+        self['unique'].update(x / t1, t1)
+        self['unique'].update(y / t2, t2)
