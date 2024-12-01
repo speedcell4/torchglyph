98aee0b2e5b6f766d65c44f98eaaa57ada5c01c3
speedcell4 <3585459+speedcell4@users.noreply.github.com>
2024-12-02 08:04:03 +0900

    Feat: Add all_gather

--- torchglyph/utils.py
+++ torchglyph/utils.py
@@ -1,8 +1,21 @@
 from pathlib import Path
+from typing import Any, List
 
-from torch import distributed
+from torch import Tensor, distributed
 
 from torchglyph.serde import load_args, load_sota, save_args, save_sota
+
+
+def all_gather_object(obj: Any) -> List[Any]:
+    object_list = [None for _ in range(distributed.get_world_size())]
+    distributed.all_gather_object(obj=obj, object_list=object_list)
+    return object_list
+
+
+def all_gather(tensor: Tensor) -> List[Tensor]:
+    tensor_list = [tensor.new_empty(size=size) for size in all_gather_object(tensor.size())]
+    distributed.all_gather(tensor=tensor, tensor_list=tensor_list)
+    return tensor_list
 
 
 def link_checkpoint(out_dir: Path, checkpoint: Path, prefix: str = 'co') -> None:
