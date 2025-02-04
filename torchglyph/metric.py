c89e35cfc076e4c180d8e836f059599b4f9bd946
speedcell4 <3585459+speedcell4@users.noreply.github.com>
2025-02-04 20:25:47 +0900

    Feat: Add MultiSacreBLEUScore

--- torchglyph/metric.py
+++ torchglyph/metric.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, List, Tuple
+
 import torch
 from torch import Tensor
 from torchmetrics import MaxMetric, MeanMetric, MetricCollection, MinMetric
@@ -81,19 +83,6 @@ class Accuracy(MeanMetric):
         return super(Accuracy, self).compute() * 100.
 
 
-class TopK(MeanMetric):
-    def __init__(self, ignore_index: int = -100) -> None:
-        super(TopK, self).__init__()
-        self.ignore_index = ignore_index
-
-    def update(self, prediction: Tensor, target: Tensor) -> None:
-        mask = (prediction == target[..., None]).any(dim=-1)
-        return super(TopK, self).update(mask.float()[target != self.ignore_index])
-
-    def compute(self) -> Tensor:
-        return super(TopK, self).compute() * 100.
-
-
 class SacreBLEUScore(_SacreBLEUScore):
     def __init__(self, lang: str):
         tokenize = {
@@ -105,3 +94,40 @@ class SacreBLEUScore(_SacreBLEUScore):
 
     def compute(self) -> Tensor:
         return super(SacreBLEUScore, self).compute() * 100.
+
+
+class MultiSacreBLEUScore(MetricCollection):
+    def __init__(self, lang_pairs: List[Tuple[str, str]]):
+        super(MultiSacreBLEUScore, self).__init__({
+            f'{lang1}-{lang2}': SacreBLEUScore(lang=lang2)
+            for lang1, lang2 in lang_pairs
+        })
+
+    def compute(self) -> Dict[str, Any]:
+        info = {**super(MultiSacreBLEUScore, self).compute()}
+        e2x, x2e, zero, avg = [], [], [], []
+        for lang_pairs, score in info.items():
+            lang1, lang2 = lang_pairs.split('-')
+
+            if lang1 == 'en':
+                e2x.append(score)
+            elif lang2 == 'en':
+                x2e.append(score)
+            else:
+                zero.append(score)
+
+            avg.append(score)
+
+        if len(e2x) > 0:
+            info['e2x'] = sum(e2x) / len(e2x)
+
+        if len(x2e) > 0:
+            info['x2e'] = sum(x2e) / len(x2e)
+
+        if len(zero) > 0:
+            info['zero'] = sum(zero) / len(zero)
+
+        if len(avg) > 0:
+            info['avg'] = sum(avg) / len(avg)
+
+        return info
