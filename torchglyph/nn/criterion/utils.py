d91b59c41268505b452db78ff609280dc04e9645
speedcell4 <3585459+speedcell4@users.noreply.github.com>
2025-02-11 15:30:03 +0900

    Feat: Update get_scaling

--- torchglyph/nn/criterion/utils.py
+++ torchglyph/nn/criterion/utils.py
@@ -1,10 +1,11 @@
-from accelerate.utils import gather_object
 from torch import distributed
+
+from torchglyph.env import all_gather_object
 
 
 def get_scaling(batch_size: int) -> float:
     if distributed.is_initialized():
-        batch_sizes = gather_object([batch_size])
+        batch_sizes = all_gather_object(batch_size)
         return sum(batch_sizes) / (len(batch_sizes) * batch_size)
 
     return 1.0
