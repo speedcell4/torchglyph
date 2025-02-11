d591d49e7ac450d24f6be551f880b58e9964d03c
speedcell4 <3585459+speedcell4@users.noreply.github.com>
2025-02-11 14:35:10 +0900

    Feat: Add load_json and dump_json

---
+++ torchglyph/env.py
@@ -0,0 +1,13 @@
+import json
+from pathlib import Path
+from typing import Any, Union
+
+
+def load_json(path: Union[Path, str]) -> Any:
+    with Path(path).open(mode='r', encoding='utf-8') as fp:
+        return json.load(fp=fp)
+
+
+def dump_json(path: Union[Path, str], obj: Any) -> None:
+    with Path(path).open(mode='w', encoding='utf-8') as fp:
+        json.dump(obj, fp=fp, indent=2, ensure_ascii=False)
